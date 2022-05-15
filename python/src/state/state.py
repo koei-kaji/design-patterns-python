068f4cfd6be9eb73b7ae79c368738c777830d0d7
koei-kaji <koei.kaji@gmail.com>
2022-05-15 13:01:17 +0900

    feat: impl state pattern with python

---
+++ python/src/state/state.py
@@ -0,0 +1,145 @@
+from __future__ import annotations
+
+import abc
+from typing import TYPE_CHECKING
+
+from ..common.model import Interface
+
+if TYPE_CHECKING:
+    from .context import ContextIF
+
+from ..common.custom_pydantic.config import BaseFrozenConfig
+from ..singleton._singleton import BaseSingleton
+from .context import ContextIF
+
+
+class StateIF(Interface):
+    @abc.abstractmethod
+    def do_clock(self, context: ContextIF, hour: int) -> None:
+        pass
+
+    @abc.abstractmethod
+    def do_use(self, context: ContextIF) -> None:
+        pass
+
+    @abc.abstractmethod
+    def do_alarm(self, context: ContextIF) -> None:
+        pass
+
+    @abc.abstractmethod
+    def do_phone(self, context: ContextIF) -> None:
+        pass
+
+    @abc.abstractmethod
+    def do_lift(self, context: ContextIF, hour: int) -> None:
+        pass
+
+
+def _switch_context_by_hour(context: ContextIF, hour: int) -> None:
+    if 12 <= hour < 13:
+        context.change_state(NoonState)
+    elif 9 <= hour < 17:
+        context.change_state(DayState)
+    elif hour < 9 or 17 <= hour:
+        context.change_state(NightState)
+
+
+class _DayState(BaseSingleton, StateIF):
+    def do_clock(self, context: ContextIF, hour: int) -> None:
+        _switch_context_by_hour(context, hour)
+
+    def do_use(self, context: ContextIF) -> None:
+        context.record_log("金庫使用（昼間）")
+
+    def do_alarm(self, context: ContextIF) -> None:
+        context.call_security_center("非常ベル（昼間）")
+        context.change_state(UrgentState)
+
+    def do_phone(self, context: ContextIF) -> None:
+        context.call_security_center("通常の通話（昼間）")
+
+    def do_lift(self, context: ContextIF, hour: int) -> None:
+        pass
+
+    def __str__(self) -> str:
+        return "[昼間]"
+
+    class Config(BaseFrozenConfig):
+        pass
+
+
+class _NightState(BaseSingleton, StateIF):
+    def do_clock(self, context: ContextIF, hour: int) -> None:
+        _switch_context_by_hour(context, hour)
+
+    def do_use(self, context: ContextIF) -> None:
+        context.call_security_center("非常：夜間の金庫使用！")
+
+    def do_alarm(self, context: ContextIF) -> None:
+        context.call_security_center("非常ベル（夜間）")
+        context.change_state(UrgentState)
+
+    def do_phone(self, context: ContextIF) -> None:
+        context.record_log("夜間の通常録音")
+
+    def do_lift(self, context: ContextIF, hour: int) -> None:
+        pass
+
+    def __str__(self) -> str:
+        return "[夜間]"
+
+    class Config(BaseFrozenConfig):
+        pass
+
+
+class _NoonState(BaseSingleton, StateIF):
+    def do_clock(self, context: ContextIF, hour: int) -> None:
+        _switch_context_by_hour(context, hour)
+
+    def do_use(self, context: ContextIF) -> None:
+        context.call_security_center("非常：昼食時の金庫使用！")
+
+    def do_alarm(self, context: ContextIF) -> None:
+        context.call_security_center("非常ベル（昼食時）")
+        context.change_state(UrgentState)
+
+    def do_phone(self, context: ContextIF) -> None:
+        context.record_log("昼食時の通常録音")
+
+    def do_lift(self, context: ContextIF, hour: int) -> None:
+        pass
+
+    def __str__(self) -> str:
+        return "[昼食時]"
+
+    class Config(BaseFrozenConfig):
+        pass
+
+
+class _UrgentState(BaseSingleton, StateIF):
+    def do_clock(self, context: ContextIF, hour: int) -> None:
+        pass
+
+    def do_use(self, context: ContextIF) -> None:
+        context.call_security_center("非常：非常時の金庫使用！")
+
+    def do_alarm(self, context: ContextIF) -> None:
+        context.call_security_center("非常ベル（非常時）")
+
+    def do_phone(self, context: ContextIF) -> None:
+        context.record_log("非常時の通常録音")
+
+    def do_lift(self, context: ContextIF, hour: int) -> None:
+        _switch_context_by_hour(context, hour)
+
+    def __str__(self) -> str:
+        return "[非常時]"
+
+    class Config(BaseFrozenConfig):
+        pass
+
+
+DayState = _DayState()
+NightState = _NightState()
+NoonState = _NoonState()
+UrgentState = _UrgentState()
